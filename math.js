6905e54be2715cc7fb1ca5b7ac0a15ca776e2930
Meijstro <35121706+Meijstro@users.noreply.github.com>
2018-01-25 11:05:01 +0100

    Add files via upload

---
+++ math.js
@@ -0,0 +1,27 @@
+
+// var cars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+// var text = "";
+// var i;
+// for (i = 0; i < number.length; i*i) {
+//     text += number[i] + "<br>";
+// }
+//
+// for (i = 0; i < number.length; i*i*i) {
+//     text += number[i] + "<br>";
+// }
+//
+// document.getElementById("table").innerHTML = text;
+// var text = "";
+// var i = 0;
+// do {
+//   text += "</br>" i , (i*i) , (i*i*i);
+//   i++;
+// }
+// while (i < 11);
+// document.getElementByID("table").innerHTML = text;
+
+var i = 0;
+while (i < 10) {
+    document.getElementById("table").innerHTML += i + "<br>";
+    i++;
+}
